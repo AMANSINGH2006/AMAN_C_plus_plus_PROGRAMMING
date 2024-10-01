33d48d377409310239c8ef457aa78ec39faaa98e
AMANSINGH2006 <aman993507@gamil.com>
2024-10-01 12:32:32 +0530

    kadanes algorithms

---
+++ imp_algos/kadanes_algo.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+void maximumSubArraySum(int* arr, int n){
+    int currSum = 0;
+    int maxSum = INT_MIN;
+    for(int i = 0; i<n; i++){
+        currSum += arr[i];
+        maxSum = max(maxSum, currSum);
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+    cout<<"Maximum Subarrays Sum : "<<maxSum;
+}
+int main(){
+    int arr[10];
+    int n;
+    cout<<"enter size of array"<<endl;
+    cin>>n;
+    cout<<"Enter the elements of arrays:"<<endl;
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    maximumSubArraySum(arr,n);
+}
