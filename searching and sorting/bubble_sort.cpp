ddc3cac2fc17ed9a226b99e8c1bd7271e8736b12
AMANSINGH2006 <aman993507@gamil.com>
2024-10-26 09:31:38 +0530

    optimized bubble sort

--- searching and sorting/bubble_sort.cpp
+++ searching and sorting/bubble_sort.cpp
@@ -8,11 +8,14 @@ void print(vector<int>ans){
 }
 vector<int> bubbleSort(vector<int>&v){
     for(int i = 0; i<v.size()-1; i++){
+        int flag = 0;
         for(int j = 0; j<v.size()-1-i; j++){
             if(v[j] > v[j+1]){
                 swap(v[j],v[j+1]);
+                flag = 1;
             }
         }
+        if(flag == 0) break;
     }
     return v;
 }
