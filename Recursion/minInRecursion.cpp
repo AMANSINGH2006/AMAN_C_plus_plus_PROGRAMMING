58681119dea9ae8b7da168782c80ba55c840dd71
AMANSINGH2006 <aman993507@gamil.com>
2024-11-09 20:06:43 +0530

    max and min using recursion

--- Recursion/minInRecursion.cpp
+++ Recursion/minInRecursion.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 #include<limits.h>
 using namespace std;
+
+int maxInArray(int arr[], int size, int index, int &maxi){
+    if(index == size){
+        return 0;
+    }
+    maxi = max(maxi,arr[index]);
+    maxInArray(arr, size, index+1, maxi);
+    return maxi;
+}
+
 int minInArray(int* arr, int size, int index, int &mini){
     if(index == size){
         return 0;
@@ -9,11 +19,14 @@ int minInArray(int* arr, int size, int index, int &mini){
     minInArray(arr, size, index + 1, mini);
     return mini;
 }
+
 int main(){
-    int arr[] = {100,34,56,23,12,45,233};
+    int arr[] = {100,34,56,23,1102,45,233};
     int size = 7;
     int index = 0;
     int mini = INT_MAX;
-    int ans = minInArray(arr, size, index, mini);
+    int maxi = INT_MIN;
+    // int ans = minInArray(arr, size, index, mini);
+    int ans = maxInArray(arr, size, index, maxi);
     cout<<"Ans = "<<ans;
 }
