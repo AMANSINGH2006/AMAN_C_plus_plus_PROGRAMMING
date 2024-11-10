6640161f1a24a3661cb58f7eb031e8d81d64619f
AMANSINGH2006 <aman993507@gamil.com>
2024-11-10 16:50:25 +0530

    find subsequence using include and exclude

---
+++ Recursion/include_exclude.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void findSubsequence(string str, int index, vector<string>&ans, string output){
+    if(index == str.length()){
+        ans.push_back(output);
+        return ;
+    }
+    char ch = str[index];
+    //include pattern of subsequence
+    findSubsequence(str,index+1,ans,output+ch);
+    //exclude pattern of subsequence
+    findSubsequence(str,index+1,ans,output);
+}
+int main(){
+    string str = "xyz";
+    vector<string>ans;
+    string output = "";
+    int index = 0;
+    findSubsequence(str,index,ans,output);
+    cout<<"The subsequences length is : "<<ans.size()<<endl;
+    for(auto s : ans){
+        cout<<s<<endl;
+    }
+}
